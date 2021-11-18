fe4436fa37e447f30115b28e4d2717ced9ba56b6
technogabriel <adangabriel1@gmail.com>
2021-11-18 20:49:32 -0300

    @RequestParam && content static

--- src/main/java/ar/com/spring/web/app/controller/EjemploParamsControl.java
+++ src/main/java/ar/com/spring/web/app/controller/EjemploParamsControl.java
@@ -1,5 +1,8 @@
 package ar.com.spring.web.app.controller;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,26 @@ public class EjemploParamsControl {
 	@GetMapping("/string")
 	public String param(@RequestParam(name="texto", required = false, defaultValue = "no ha ingresado nada") String texto, Model model) {
 		model.addAttribute("resultado", "el parametro enviado es: " + texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer num, Model model) {
+		model.addAttribute("resultado", "el parametro enviado es: '" + saludo + "' y el numero es: '" + num +"'");
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-paramsRequest")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer num=null;
+		try {
+		 num= Integer.parseInt(request.getParameter("num"));
+		}catch(NumberFormatException nfe){
+			num=0;
+		}
+		
+		model.addAttribute("resultado", "el parametro enviado es: '" + saludo + "' y el numero es: '" + num +"'");
 		return "params/ver";
 	}
 
